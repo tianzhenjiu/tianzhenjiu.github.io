0ba82768d06b5ab28092a66439e5b8c442847b1e
tianzhenjiu <tianzhenjiu@163.com>
2023-12-20 20:57:16 +0800

    update turn servers

--- network.js
+++ network.js
@@ -1,6 +1,12 @@
 var configuration = {
     "iceServers": [
         {
+            {
+            "urls": ["turn:freeturn.net:3478"],
+            "username": "free",
+            "credential": "free"
+        },
+            
             "urls": [
                 "stun:stun.l.google.com:19302",
                 "stun:stun1.l.google.com:19302"
